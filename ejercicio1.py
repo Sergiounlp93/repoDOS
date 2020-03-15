4281bf9a97367293119eb5ff2ae442096e493f10
Sergiounlp93 <ser.unlp93@gmail.com>
2020-03-15 19:56:33 -0300

    ejercicio1.py modificado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,16 @@
-print("Hola mundo")
+"""
+Dado el siguiente string, generar una lista donde
+cada elemento sea una palabra,
+utilizando el espacio ' ' como separador:
+
+frase = "Si trabajás mucho con computadoras,
+eventualmente encontrarás que te gustaría automatizar alguna tarea.
+Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número de archivos de texto,
+ o renombrar y reorganizar un montón de archivos con fotos de una manera compleja.
+ Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada
+ con interfaz gráfica, o un juego simple."
+"""
+
+frase = "Si trabajás mucho con computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número de archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o un juego simple."
+palabras = frase.split(" ")
+print(palabras)
