f269ebaca3b08c547180c9f72a73d9f6412ebb86
Sergiounlp93 <ser.unlp93@gmail.com>
2020-03-15 20:07:25 -0300

    se agregó ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+"""
+    Genere una nueva lista con todas las palabras de la frase dada en el ejercicio 1 en mayúsculas
+"""
+frase = "Si trabajás mucho con computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número de archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o un juego simple."
+fraseMayus = frase.upper()
+palabrasMayus = fraseMayus.split(" ");
+print(palabrasMayus)
