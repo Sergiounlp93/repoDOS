ea8e43ecb0860e29af30a240da993ba1ff3532ce
Sergiounlp93 <ser.unlp93@gmail.com>
2020-03-19 10:04:24 -0300

    Se cambio el uso de una estructura para procesar componentes por el uso de un tipo conjunto con el fin de simplificar la implementacion

--- ejercicio5.py
+++ ejercicio5.py
@@ -6,7 +6,8 @@ repetidas e indistintamente en mayúsculas y minúsculas,
 lista=[]
 frase = "Si trabajás mucho CON computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número DE archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o UN juego simple."
 palabras = frase.split()
-for p in palabras:
-    if palabras.count(p)<2:
-        lista.append(p)
-print(lista)
+conjunto = set(palabras)
+nuevaLista = list(conjunto)
+print(f"la lista es :\n\n  {palabras}")
+print(f"el conjunto es :\n\n   {conjunto}")
+print(f"la nueva lista es :\n\n   {nuevaLista}")
