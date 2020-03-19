5f867cc0b1c347c49342bfc417f4718836b6931a
Sergiounlp93 <ser.unlp93@gmail.com>
2020-03-19 09:39:54 -0300

    se cambiaron las comillas del print

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,5 +12,5 @@ Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran núme
 """
 
 frase = "Si trabajás mucho con computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número de archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o un juego simple."
-palabras = frase.split(" ")
+palabras = frase.split(' ')
 print(palabras)
