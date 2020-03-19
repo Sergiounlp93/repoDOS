d4e1a29beebc6fbc37c0ba956e880d2f941f7922
Sergiounlp93 <ser.unlp93@gmail.com>
2020-03-19 10:09:39 -0300

    Se cambio el uso de una estructura para procesar componentes por el uso de un tipo conjunto con el fin de simplificar la implementacion ademas se agrego un lower para que queden todas las palabras iguales

--- ejercicio5.py
+++ ejercicio5.py
@@ -5,7 +5,7 @@ repetidas e indistintamente en mayúsculas y minúsculas,
 """
 lista=[]
 frase = "Si trabajás mucho CON computadoras, eventualmente encontrarás que te gustaría automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y reemplazo en un gran número DE archivos de texto, o renombrar y reorganizar un montón de archivos con fotos de una manera compleja. Tal vez quieras escribir alguna pequeña base de datos personalizada, o una aplicación especializada con interfaz gráfica, o UN juego simple."
-palabras = frase.split()
+palabras = frase.lower().split()
 conjunto = set(palabras)
 nuevaLista = list(conjunto)
 print(f"la lista es :\n\n  {palabras}")
